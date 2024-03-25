97ab5912207f8bdd84555ab04615193a646fabb9
Christian <chrinoemi12@gmail.com>
2024-03-25 14:59:33 +0100

    Frontend

--- js/dom-manipulator/createlist.js
+++ js/dom-manipulator/createlist.js
@@ -4,26 +4,43 @@
 @returns} movies 
  */
 
-        export const createTrendingMoviesList = (movies) => {
-            const cardContainer = document.getElementById("movies-cards");
-            movies.forEach(element => {
-                //Creiamo i nodi
-                const card = document.createElement("div");
-                const textContainer = document.createElement("div");
-                const image = document.createElement("img");
-                image.src = `https://image.tmdb.org/t/p/w500${element.poster_path};`
-                image.alt = "poster";
-                const title = document.createElement("h1");
-                title.innerText = element.title;
-                const description = document.createElement("p");
-                description.innerText = element.overview;
+//crea una lista dei film di tendenza
+export const createTrendingMoviesList = (movies, cardId) => {
+    const cardContainer = document.getElementById(cardId);
+    movies.forEach(element => {
+        const card = document.createElement("div");
+        card.className = "col-12 col-md-6 col-1g-4 bg-transparent";
+        const cardInner = document.createElement("div");
+        cardInner.className = "card flex-column h-100";
+        const cardImage = document.createElement("img");
+        cardImage.alt = "backdrop";
+        cardImage.className = "card-img-top";
+        const textContainer = document.createElement("div");
+        textContainer.className = "card-body";
+        const cardTitle = document.createElement("h5");
+        cardTitle.className = "card-title";
+        const cardDescription = document.createElement("p");
+        cardDescription.className = "card-text";
+        const cardButtonContainer = document.createElement("div");
+        cardButtonContainer.className = "card-button-container";
+        const cardButton = document.createElement("a");
+        cardButton.innerHTML =  "Learn More";
+        cardButton.className = "btn btn-primary";
         
-        
-                //Appendiamo i nodi al DOM
-                textContainer.appendChild(title);
-                textContainer.appendChild(description);
-                card.appendChild(textContainer);
-                card.appendChild(image);
-                cardContainer.appendChild(card);
-            });
-        }
+
+        cardImage.src = `https://image.tmdb.org/t/p/w500${element.backdrop_path}`;
+        cardTitle.textContent = element.title;
+        cardDescription.textContent = element.overview;
+
+        // Aggiungo gli elementi al contenitore principale
+        textContainer.appendChild(cardTitle); 
+        textContainer.appendChild(cardDescription); 
+        cardInner.appendChild(cardImage); 
+        cardInner.appendChild(textContainer) 
+        card.appendChild(cardInner);
+        cardButtonContainer.appendChild(cardButton); 
+        cardInner.appendChild(cardButtonContainer); 
+        cardContainer.appendChild(card);
+
+    });
+}
